84aad6de3380da9746bc56dff81eb4b5be7558b8
carolchennn <wuchencarol@gmail.com>
2014-06-27 15:02:31 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Script variables
 var timelineConfig = {
-	key: '0AhVQ7rH5MwaHdDA1ekNFU0d1Vm9lREdzN0tsd0czS3c',
+	key: '0AhVQ7rH5MwaHdDA1ekNFU0d1Vm9lREdzN0tsd0czS3c#gid=0',
 	sheetName: 'Posts' // change to name of spreadsheet 'sheet' that contains the data
 }
  
